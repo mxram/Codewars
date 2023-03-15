13746ef297002e60723bc40c5da838d86b41e13c
Mikhail Baranov <msbaranov@icloud.com>
2023-03-15 11:34:10 +0300

    fix code style

--- 6 kyu/Tribonacci Sequence/Tribonacci Sequence.playground/Contents.swift
+++ 6 kyu/Tribonacci Sequence/Tribonacci Sequence.playground/Contents.swift
@@ -19,13 +19,13 @@ func tribonacci2(_ signature: [Int], _ n: Int) -> [Int] {
 }
 
 func tribonacci3(_ signature: [Int], _ n: Int) -> [Int] {
-  guard n > 0 else { return [] }
-  guard n > 3 else { return [] + signature.prefix(n) }
-  var result = signature
-  for _ in 3..<n {
-    result.append(result.suffix(3).reduce(0, +))
-  }
-  return result
+    guard n > 0 else { return [] }
+    guard n > 3 else { return [] + signature.prefix(n) }
+    var result = signature
+    for _ in 3..<n {
+        result.append(result.suffix(3).reduce(0, +))
+    }
+    return result
 }
 
 // MARK: Tests
